86503c1a97754a44d8da131a44fa9a48ebc8e63c
MichaelFrediani <fredianimichael@gmail.com>
2023-11-27 16:43:16 -0500

    sql connect to make test

--- sql_connect.py
+++ sql_connect.py
@@ -4,20 +4,20 @@ from sklearn.linear_model import SGDClassifier
 import sqlite3 as sql
 
 # change this to your LOCAL path to the CSV file. that way you use what's in the google drive @mike 
-excel_file = './LabelledData_2/2.xlsx'
+excel_file = './UNSW-NB15_Labelled_Set_2.xlsx'
 data = pd.read_excel(excel_file)
 
 # connect to sql lite databse 
-connection = sql.connect('smaller-train_pkts.db')
+connection = sql.connect('test.db')
 
 # create a cursor object - "cursor is a control structure that enables traversal over the records in a database"
 cusor = connection.cursor()
 
 # Convert the data frame to sql table
-data.to_sql('train_nids_1', connection, if_exists='replace', index=False)
+data.to_sql('test_1', connection, if_exists='replace', index=False)
 
 # read the sql table
-data = pd.read_sql('SELECT * FROM train_nids_1', connection)
+data = pd.read_sql('SELECT * FROM test_1', connection)
 
 # commit the changes
 connection.commit()
