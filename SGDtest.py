19bd7465faf43b8227a87dd975565a956eb4ef09
MichaelFrediani <fredianimichael@gmail.com>
2023-11-27 18:27:31 -0500

    We have a learned machine?

--- SGDtest.py
+++ SGDtest.py
@@ -8,11 +8,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import sqlite3 as sql
 import numpy as np
+import gc
 
 # copy code from ArrayTest.py
 
 # create connection
-connection = sql.connect('training.db')
+connection = sql.connect('train.db')
 
 # create cursor
 cursor = connection.cursor()
@@ -37,27 +38,36 @@ included_columns = ['Src_ip_A', 'Src_ip_B', 'Src_ip_C', 'Src_ip_D', 'Source_Port
 SQL_select = ', '.join(included_columns)
 
 # seems to be some edge case where it gets rows that are filled with None? so have it not include those
-execute = f"SELECT {SQL_select} FROM train_1;"
+execute = f"SELECT {SQL_select} FROM train_1 LIMIT 100000;"
 cursor.execute(execute)
+
 
 # get the result of that execute. this will be the 2D array with every column except flag
 result_features = cursor.fetchall()
 
+print("47")
 # let's repeat but for a one dimensional array
 # we want the flag column (or label, we can easily switch it out)
 # create execute string
-execute = f"SELECT Flag FROM train_1;"
+execute = f"SELECT Flag FROM train_1 LIMIT 100000;"
 cursor.execute(execute)
 
 result_label = cursor.fetchall()
 
+print("56")
 # we can convert the result to a array
 # this array is a 2D array if sample number and feature, on sprint planning this is 3.d.i
+
+gc.collect()
+
+
 features_train = np.array(result_features)
 
 # this array is a 1d array of labels, on sprint planning this is 3.d.ii
 labels_train = np.array(result_label)
 
+
+print("65")
 # print("Below is y array. 2D array of samples and features")
 # print(features)
 # print("Below is x array. 1D array of labels")
@@ -78,18 +88,30 @@ connection.close()
 # learn machine
 clf = SGDClassifier() # running it raw with default parameters
 
-print(features_train[0,:])
-print(labels_train)
-# train the data
-# Error I get: ValueError: could not convert string to float: '149.171.126.9'
-# We need to make sure everything is a string? Everything is a float? I'm not sure
-# I am taking lunch
+# print(features_train[0,:])
+# print(labels_train)
 
+
+# Assuming features_train is your NumPy array
+
+# # Check for NaN values in the NumPy array
+# nan_mask = np.isnan(features_train.astype(float))
+
+# # Sum the NaN values along each column
+# nan_sum_per_column = np.sum(nan_mask, axis=0)
+
+# # Print the count of NaN values for each column
+# print("Number of NaN values per column:")
+# print(nan_sum_per_column)
+
+# train the data
 
 clf.fit(features_train, labels_train.ravel()) # .ravel() is to get rid of a warning, it's not important
 
 # make predictions
 # y_pred = clf.predict(X_test)
+
+# print(y_pred)
 
 # # get accuracy
 # accuracy = clf.score(y_test, y_pred)
